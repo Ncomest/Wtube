bb41e604900b529aaaba03fb8e5f1f6e46bec5e5
Hurtik Igor <hurtik.igor@gmail.com>
2023-10-25 17:37:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Route, Routes, Link } from "react-router-dom";
+
+import Home from "./Pages/Home/Home";
+import Movies from "./Pages/Movies/Movies";
+import User from "./Pages/User/User";
+import Error from "./Pages/Error/Error";
+
+function App() {
+ return (
+  <>
+  <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/movies">Movies</Link></li>
+      <li><Link to="/user">User</Link></li>
+    </ul>
+  </nav>
+
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/Movies" element={<Movies />} />
+    <Route path="/User" element={<User />} />
+    <Route path="*" element={<Error />} />
+
+   </Routes>
+  </>
+ );
+}
+
+export default App;
