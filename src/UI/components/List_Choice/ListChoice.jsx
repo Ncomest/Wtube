7daf4452f28144361e43ec4c32b031b455ea3d61
Hurtik Igor <hurtik.igor@gmail.com>
2023-11-09 01:43:59 +0300

    add option year and country

--- src/UI/components/List_Choice/ListChoice.jsx
+++ src/UI/components/List_Choice/ListChoice.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import "./ListChoice.css";
 
-function ListChoice() {
- return <div>ListChoice</div>;
+function ListChoice({ children }) {
+ return <div>{children}</div>;
+}
+
+export function Genre() {
+ return (
+  <>
+   <p>Жанр</p>
+   <select name="Genre" id="genre">
+    <option value="0">Боевик</option>
+    <option value="1">Комедия</option>
+    <option value="2">Триллер</option>
+   </select>
+  </>
+ );
+}
+export function Country() {
+ return (
+  <>
+   <p>Страна</p>
+   <select name="Country" id="country">
+    <option value="0">Россия</option>
+    <option value="1">Америка</option>
+    <option value="2">Китай</option>
+   </select>
+  </>
+ );
 }
 
 export default ListChoice;
--- src/modules/components/SideBar/SideBar.jsx
+++ src/modules/components/SideBar/SideBar.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import "./SideBar.css";
-
+import ListChoice, {
+ Country,
+ Genre,
+} from "../../../UI/components/List_Choice/ListChoice";
 
 function SideBar() {
- return <div className="SideBar">SideBar</div>;
+ return (
+  <div className="SideBar">
+   <FilterBar />
+  </div>
+ );
 }
 
+const FilterBar = () => {
+ return (
+  <div>
+   <ListChoice>
+    <Genre />
+   </ListChoice>
+   <ListChoice>
+    <Country />
+   </ListChoice>
 
-const SideBarBackground = () => {
-  return (
-    <div></div>
-  );
-}
+   <div>Год</div>
+   <div>Рейтинг имдб</div>
+   <div>рейтинг втубе</div>
+   <div>кнопки</div>
+  </div>
+ );
+};
 
 export default SideBar;
