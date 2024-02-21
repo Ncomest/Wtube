f58e9aceafdb964e8c4cdaeeac3a8b4daeed7974
Hurtik Igor <hurtik.igor@gmail.com>
2024-02-21 17:36:35 +0300

    create form structure

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./User.css";
 
 function User() {
- return <div style={{ height: "100vh" }}>User</div>;
+ return (
+  <div className="user">
+   <div className="user-form">
+    <form action="" className="form-login">
+     <label htmlFor="login">Login</label>
+     <input type="text" id="login" name="login" />
+
+     <label htmlFor="password">Password</label>
+     <input type="password" id="password" name="password" />
+
+     <div className="checkbox-login">
+      <input id="check" type="checkbox" />
+      <label htmlFor="check">I agree with the rules</label>
+     </div>
+
+     <button type="submit">Log In</button>
+    </form>
+   </div>
+  </div>
+ );
 }
 
 export default User;
