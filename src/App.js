645bd35b31abccd3ca70dec39099c023823ff025
Hurtik Igor <hurtik.igor@gmail.com>
2024-02-29 18:33:23 +0300

    add btn lang

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import Footer from "./modules/Footer/Footer";
 import User from "./Pages/User/User";
 import UserPofile from "./Pages/User/UserProfile/UserPofile";
 import ActorsCast from "./Pages/ActorsCast/ActorsCast";
+import Language from "./UI/components/LangBar/Language";
 
 function App() {
  return (
   <>
+   <Language />
    <Header />
    <Routes>
     <Route path="/" element={<Home />} />
---
+++ src/UI/components/LangBar/Language.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Language.css";
+
+const Language = () => {
+ return (
+  <div className="language">
+   <select name="" id="">
+    <option value="en-US">EN</option>
+    <option value="ru-RU">RU</option>
+   </select>
+  </div>
+ );
+};
+
+export default Language;
