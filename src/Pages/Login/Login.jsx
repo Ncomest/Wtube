7e13c296be6af0942af684304f907f7cdf14ce29
Hurtik Igor <hurtik.igor@gmail.com>
2024-03-04 23:53:59 +0300

    sketch login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
- return <div className="login">Login</div>;
+ return (
+  <div className="login">
+   <form action="">
+    <label htmlFor="">
+     <input type="text" />
+    </label>
+    <button>Login</button>
+   </form>
+   <button>go to Sign Up</button>
+  </div>
+ );
 }
 
 export default Login;
