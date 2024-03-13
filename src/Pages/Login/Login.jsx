d440bdc21db7ff4872fd658cea1189ab1485f9e5
Hurtik Igor <hurtik.igor@gmail.com>
2024-03-14 00:28:19 +0300

    writing login forn

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,25 +1,50 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Login.css";
 
 function Login() {
  const userRef = useRef();
- const [pwd, setPwd] = useState("");
  const [userData, setUserData] = useState({
-  username: "",
-  password: "",
+  user: "",
+  pwd: "",
  });
+ //  const [success, setSuccess] = useState(false);
 
- const userDataStorage = JSON.parse(localStorage.getItem("userData")) || [];
- console.log(userDataStorage);
+ const handleInputChange = (e) => {
+  const { name, value } = e.target;
+  setUserData((prevUserData) => ({ ...prevUserData, [name]: value }));
+ };
 
+ const handleLogin = (e) => {
+  e.preventDefault();
+  const userDataStorage = JSON.parse(localStorage.getItem("userData")) || [];
+  const { user, pwd } = userData;
+  const userFind = userDataStorage.find((data) => data.user === user);
+  if (userFind && userFind.pwd === pwd) {
+   console.log("success");
+  } else {
+   console.log("failrue");
+  }
+ };
 
  return (
   <div className="login">
-   <form className="login-form">
+   <form className="login-form" onSubmit={handleLogin}>
     <label htmlFor="username">Username:</label>
-    <input type="text" name="username" />
+    <input
+     type="text"
+     name="user"
+     value={userData.user}
+     onChange={handleInputChange}
+     required
+    />
     <label htmlFor="password">Password:</label>
-    <input type="password" name="password" />
+    <input
+     type="password"
+     name="pwd"
+     value={userData.pwd}
+     onChange={handleInputChange}
+     required
+    />
     <button className="login-btn">Login</button>
     <button>go to Sign Up</button>
    </form>
