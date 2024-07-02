4c36532da2a753de83eeb134106828d6c4b307e3
Hurtik Igor <hurtik.igor@gmail.com>
2024-07-02 23:01:31 +0300

    fix lang sort

--- src/UI/components/Buttons/Filter_Menu/Button_Filter_Menu.jsx
+++ src/UI/components/Buttons/Filter_Menu/Button_Filter_Menu.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Button_Filter_Menu.css";
-
+import { useTranslation } from "react-i18next";
 
 function ButtonFilterMenu({ sortMovies, setSortMovies }) {
+ const { t } = useTranslation();
  const handleSort = (e) => {
   setSortMovies(e.target.value);
  };
- console.log(sortMovies);
  return (
   <>
    <select
@@ -16,12 +16,12 @@ function ButtonFilterMenu({ sortMovies, setSortMovies }) {
     onChange={handleSort}
     value={sortMovies}
    >
-    <option value="primary_release_date.desc">по дате: по убыванию</option>
-    <option value="primary_release_date.asc">по дате: по возрастанию</option>
-    <option value="vote_average.desc">по рейтингу: по убыванию</option>
-    <option value="vote_average.asc">по рейтингу: по возрастанию</option>
-    <option value="popularity.desc">по популярности: по убыванию</option>
-    <option value="popularity.asc">по популярности: по возрастанию</option>
+    <option value="primary_release_date.desc">{t("byDateDescending")}</option>
+    <option value="primary_release_date.asc">{t("byDateAscending")}</option>
+    <option value="vote_average.desc">{t("byRatingDescending")}</option>
+    <option value="vote_average.asc">{t("byRatingAscending")}</option>
+    <option value="popularity.desc">{t("byPopularDescending")}</option>
+    <option value="popularity.asc">{t("byPopularAscending")}</option>
    </select>
   </>
  );
